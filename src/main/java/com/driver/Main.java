bf195d24110515b93b21eb3946079b16ceb5cbd8
vishnu <vishnuv9677@gmail.com>
2023-04-15 10:34:10 +0530

    Assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
+
+     public static void main(String[] args) {
+        B b=new B();
+        String ans=b.meth();
+        System.out.println(ans);
+     }
   
 }
